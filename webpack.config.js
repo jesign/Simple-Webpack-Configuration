05bc5465ccc0bc6471efcac7d54aeb08399d180e
jesign <jixzignacio@gmail.com>
2017-09-30 07:05:23 +0800

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var path = require('path');
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: {
+		app: './src/app.js',
+		app: './src/app.scss'
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			/* Sass Compiler */
+			{
+				test: /\.s[ac]ss$/,
+				use: ExtractTextPlugin.extract({
+		          	use : ['css-loader', 'sass-loader'],
+		          	fallback: "style-loader",
+		        })
+				
+			},
+			/* Images Url*/
+			{
+				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+				loader : 'file-loader',
+				options : {
+					name: 'images/[name].[hash].[ext]'
+				}
+			},
+			/* ES5*/
+			{ 
+				test: /\.js$/, 
+				exclude: /node_modules/, 
+				loader: "babel-loader" 
+			},
+		]
+	},
+	plugins: [
+	  	new ExtractTextPlugin("[name].css"),
+	]
+}
+
+if(process.env.NODE_ENV === 'production'){
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin()
+	);
+}
